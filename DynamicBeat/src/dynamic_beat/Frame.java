3c352ba6f76d9f83f780d81841fb9120c8156ade
mikael1017 <59891471+mikael1017@users.noreply.github.com>
2020-11-03 16:22:54 -0800

    Implemented 3 levels to choose

--- DynamicBeat/src/dynamic_beat/Frame.java
+++ DynamicBeat/src/dynamic_beat/Frame.java
@@ -1,8 +1,8 @@
 package dynamic_beat;
 
-
 import java.awt.event.MouseEvent;
 import java.awt.Cursor;
+import java.awt.Image;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseMotionAdapter;
 
@@ -23,13 +23,25 @@ public class Frame extends JFrame {
     private ImageIcon quitButtonImage = new ImageIcon("images/quitbutton.png");
     private ImageIcon quitButtonClickedImage = new ImageIcon("images/quitbuttonEntered.png");
     private ImageIcon backButtonImage = new ImageIcon("images/backbutton.png");
+    private ImageIcon easyImage = new ImageIcon("images/rm.jpg");
+    private ImageIcon mediumImage = new ImageIcon("images/mu.jpg");
+    private ImageIcon hardImage = new ImageIcon("images/barcelona.jpg");
 
     private JButton backButton = new JButton(backButtonImage);
     private JButton exitButton = new JButton(exitButtonImage);
     private JButton startButton = new JButton(startButtonImage);
     private JButton quitButton = new JButton(quitButtonImage);
+    private JLabel easy = new JLabel(easyImage);
+    private JLabel medium = new JLabel(mediumImage);
+    private JLabel hard = new JLabel(hardImage);
+    
+    private JButton easyLabel = new JButton(new ImageIcon("images/easylabel.png"));
+    private JButton mediumLabel = new JButton(new ImageIcon("images/mediumlabel.png"));
+    private JButton hardLabel = new JButton(new ImageIcon("images/hardlabel.png"));
 
+    private Image selectedImage = new ImageIcon().getImage();
     private Music currMusic = new Music("introMusic.mp3", true);
+    private boolean isMainScreen = false;
 
     private int mouseX, mouseY;
     public Frame() {
@@ -44,6 +56,8 @@ public class Frame extends JFrame {
         exitButton.setBorderPainted(false);
         exitButton.setContentAreaFilled(false);
         exitButton.setFocusPainted(false);
+        
+        
         //  add event handling of exit button icon
         exitButton.addMouseListener(new MouseAdapter() {
 
@@ -70,6 +84,8 @@ public class Frame extends JFrame {
         startButton.setBorderPainted(false);
         startButton.setContentAreaFilled(false);
         startButton.setFocusPainted(false);
+        
+        
         //  add event handling of start button icon
         startButton.addMouseListener(new MouseAdapter() {
 
@@ -99,6 +115,8 @@ public class Frame extends JFrame {
         quitButton.setBorderPainted(false);
         quitButton.setContentAreaFilled(false);
         quitButton.setFocusPainted(false);
+        
+        
         //  add event handling of quit button icon
         quitButton.addMouseListener(new MouseAdapter() {
 
@@ -165,7 +183,7 @@ public class Frame extends JFrame {
             }
         });
 
-
+        drawLevels();
         add(backButton);
         add(exitButton);
         add(startButton);
@@ -185,6 +203,14 @@ public class Frame extends JFrame {
         exitButton.setVisible(false);
         quitButton.setVisible(false);
         backButton.setVisible(true);
+        easy.setVisible(true);
+        medium.setVisible(true);
+        hard.setVisible(true);
+        easyLabel.setVisible(true);
+        mediumLabel.setVisible(true);
+        hardLabel.setVisible(true);
+        isMainScreen = true;
+
     }
 
     public void mainPage() {
@@ -193,7 +219,116 @@ public class Frame extends JFrame {
         exitButton.setVisible(true);
         quitButton.setVisible(true);
         backButton.setVisible(false);
+        easy.setVisible(false);
+        medium.setVisible(false);
+        hard.setVisible(false);
+        easyLabel.setVisible(false);
+        mediumLabel.setVisible(false);
+        hardLabel.setVisible(false);
     }
+
+    public void drawLevels() {
+        easyLabel.setBounds(120, 100, 200, 160);
+        mediumLabel.setBounds(120, 300, 200, 160);
+        hardLabel.setBounds(120, 500, 200, 160);
+
+        easy.setBounds(120, 100, 200, 160);;
+        easyLabel.setBorderPainted(false);
+        easyLabel.setContentAreaFilled(false);
+        easyLabel.setFocusPainted(false);
+
+        medium.setBounds(120, 300, 200, 160);
+        mediumLabel.setBorderPainted(false);
+        mediumLabel.setContentAreaFilled(false);
+        mediumLabel.setFocusPainted(false);
+
+        hard.setBounds(120, 500, 200, 160);
+        hardLabel.setBorderPainted(false);
+        hardLabel.setContentAreaFilled(false);
+        hardLabel.setFocusPainted(false);
+
+        easyLabel.addMouseListener(new MouseAdapter() {
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                easyLabel.setCursor(new Cursor(Cursor.HAND_CURSOR));
+            }
+            
+            @Override
+            public void mouseExited(MouseEvent e) {
+                easyLabel.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+                easyGame(); 
+            }
+        });
+
+        mediumLabel.addMouseListener(new MouseAdapter() {
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                mediumLabel.setCursor(new Cursor(Cursor.HAND_CURSOR));
+            }
+            
+            @Override
+            public void mouseExited(MouseEvent e) {
+                mediumLabel.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+                mediumGame(); 
+            }
+        });
+
+        hardLabel.addMouseListener(new MouseAdapter() {
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                hardLabel.setCursor(new Cursor(Cursor.HAND_CURSOR));
+            }
+            
+            @Override
+            public void mouseExited(MouseEvent e) {
+                hardLabel.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+                hardGame(); 
+            }
+        });
+        add(easyLabel);
+        add(mediumLabel);
+        add(hardLabel);
+        add(easy);
+        add(medium);
+        add(hard);
+        easy.setVisible(false);
+        medium.setVisible(false);
+        hard.setVisible(false);
+        easyLabel.setVisible(false);
+        mediumLabel.setVisible(false);
+        hardLabel.setVisible(false);
+        
+    }
+
+    public void easyGame() {
+
+    }
+
+    public void mediumGame() {
+
+    }
+
+    public void hardGame() {
+
+    }
+
+
+
 
 
 }
